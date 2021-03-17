ceefa503e37fec9916efe85af4cc8e7068c8aac7
Joost Laoh <joost.laoh@outlook.com>
2021-03-17 17:29:37 +0100

    made it a maven project

---
+++ src/main/java/com/joost/opt2/Afspraak.java
@@ -0,0 +1,16 @@
+package com.joost.opt2;
+
+import java.time.LocalDateTime;
+
+class Afspraak {
+    private LocalDateTime start;
+    private LocalDateTime end;
+    public Afspraak(LocalDateTime start, LocalDateTime end) {
+
+    }
+}
+class Test {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/test/java/com/joost/opt2/AppTest.java
@@ -0,0 +1,20 @@
+package com.joost.opt2;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
